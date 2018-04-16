ffe5599421c47728ab63eb41d1b5f78b5ab6c76e
DamianWiatrzyk <D.Wiatrzyk@hotmail.com>
2018-04-16 19:18:17 +0200

    SerwoTest

--- src/main.c
+++ src/main.c
@@ -13,6 +13,7 @@
 #include "stm32f4_discovery.h"
 
 void RGB();
+void SetSERWO();
 
 uint8_t CardID[5];
 
@@ -22,7 +23,7 @@ int a = 0;
 
 int main(void) {
 	/////////////////////////////////////////////////////////////////////
-
+/* Inicjalizacja timerów dla diody */
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
 
 	GPIO_InitTypeDef GPIO_InitStructure;
@@ -64,13 +65,46 @@ int main(void) {
 			TIM_OC3Init(TIM1, &TIM_OCInitStructure);
 			TIM_OC3PreloadConfig(TIM1, TIM_OCPreload_Enable);
 
-			TIM_OC4Init(TIM1, &TIM_OCInitStructure);
-			TIM_OC4PreloadConfig(TIM1, TIM_OCPreload_Enable);
 
 			TIM1->CCR1 = 8400;
 			TIM1->CCR2 = 8400;
 			TIM1->CCR3 = 8400;
 			///////////////////////////////////////////
+			/* PWM dla Serwa */
+			RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+				GPIO_InitTypeDef GPIO_InitStructure;
+						GPIO_PinAFConfig(GPIOA, GPIO_PinSource15, GPIO_AF_TIM2);
+						GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;
+						GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+						GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+						GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+						GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+						GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+						///timer
+						RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+
+						TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+						TIM_TimeBaseStructure.TIM_Period = 8399;
+						TIM_TimeBaseStructure.TIM_Prescaler = 9;
+						TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+						TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+						TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+
+						TIM_Cmd(TIM2, ENABLE); //uruchomienie
+
+						TIM_OCInitTypeDef TIM_OCInitStructure;
+						/* PWM1 Mode configuration: */
+						TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+						TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+						TIM_OCInitStructure.TIM_Pulse = 0;
+						TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+
+						TIM_OC1Init(TIM2, &TIM_OCInitStructure);
+						TIM_OC1PreloadConfig(TIM2, TIM_OCPreload_Enable);
+
+			TIM2->CCR1 = 8400;
 
 	TM_MFRC522_Init();
 	for (;;) {/*
