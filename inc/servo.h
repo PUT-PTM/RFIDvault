ffbe0c44df6601752ab06790abf7658d4ddafde1
Andrzej Dybowski <dybowski@andrzejd.pl>
2018-05-11 15:53:28 +0200

    biblioteka serwomechanizmu: 	dodano: 		- servoInit() 		- servoSetDuty()

--- inc/servo.h
+++ inc/servo.h
@@ -1,3 +1,19 @@
 #ifndef __servo_H
 #define __servo_H
+
+#define SERVO_TIMER_RCC RCC_APB1Periph_TIM2
+#define SERVO_TIMER_PERIOD 8400
+#define SERVO_TIMER_PRESCALER 10
+#define SERVO_TIMER TIM2
+#define SERVO_TIMER_GPIO GPIO_AF_TIM2
+#define SERVO_TIMER_CHANEL TIM_OCMode_PWM1
+
+#define SERVO_GPIO_RCC RCC_AHB1Periph_GPIOA
+#define SERVO_GPIO_PORT GPIOA
+#define SERVO_GPIO_PIN_SOURCE GPIO_PinSource15
+#define SERVO_GPIO_PIN GPIO_Pin_15
+
+void servoInit();
+void servoSetDuty(unsigned int duty);
+
 #endif /* __servo_H */
---
+++ src/servo.c
@@ -0,0 +1,41 @@
+#include <servo.h>
+#include "stm32f4xx.h"
+
+void servoInit() {
+	RCC_AHB1PeriphClockCmd(SERVO_GPIO_RCC, ENABLE);
+
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_PinAFConfig(SERVO_GPIO_PORT, SERVO_GPIO_PIN_SOURCE, SERVO_TIMER_GPIO);
+	GPIO_InitStructure.GPIO_Pin = SERVO_GPIO_PIN;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+	GPIO_Init(SERVO_GPIO_PORT, &GPIO_InitStructure);
+
+	///timer
+	RCC_APB1PeriphClockCmd(SERVO_TIMER_RCC, ENABLE);
+
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+	TIM_TimeBaseStructure.TIM_Period = SERVO_TIMER_PERIOD - 1;
+	TIM_TimeBaseStructure.TIM_Prescaler = SERVO_TIMER_PRESCALER - 1;
+	TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+	TIM_TimeBaseInit(SERVO_TIMER, &TIM_TimeBaseStructure);
+
+	TIM_Cmd(SERVO_TIMER, ENABLE); //uruchomienie
+
+	TIM_OCInitTypeDef TIM_OCInitStructure;
+	/* PWM1 Mode configuration: */
+	TIM_OCInitStructure.TIM_OCMode = SERVO_TIMER_CHANEL;
+	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+	TIM_OCInitStructure.TIM_Pulse = 0;
+	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+
+	TIM_OC1Init(SERVO_TIMER, &TIM_OCInitStructure);
+	TIM_OC1PreloadConfig(SERVO_TIMER, TIM_OCPreload_Enable);
+}
+
+void servoSetDuty(unsigned int duty) {
+	TIM2->CCR1 = (uint32_t) (SERVO_TIMER_PERIOD) * (duty / 100);
+}
