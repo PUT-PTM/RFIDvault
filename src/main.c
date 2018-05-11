0c0f9aabe8681985b62ea5cfcfd54b736658bc83
Andrzej Dybowski <dybowski@andrzejd.pl>
2018-05-11 14:54:53 +0200

    zczytanie flash beta

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 /**
  ******************************************************************************
  * @file    main.c
- * @author  Ac6
+ * @author  Damian Wiatrzyk, Andrzej Dybowski
  * @version V1.0
  * @date    01-December-2013
  * @brief   Default main function.
@@ -13,29 +13,33 @@
 #include "stm32f4_discovery.h"
 #include "stm32f4_flash.h"
 
-void RGB();
+//void RGB();
 //void SetSERWO();
 
 uint8_t CardID[5];
 
 uint8_t CardID_a, CardID_b, CardID_c, CardID_d, CardID_e;
 
-int a = 0;
+int codeFlash;
 
 uint16_t data;
 int main(void) {
-	Internal_FLASH_Init();
+	codeFlash = Internal_FLASH_Init();
 
-	uint16_t writeData = 1;
+	if (codeFlash == 1) {
+		uint16_t writeData = 1;
 
-	Internal_FLASH_Write(0x5555, writeData);
-	data = Internal_FLASH_Read(0x5555);
-	while(1);
+		codeFlash = Internal_FLASH_Write(1, writeData);
+		data = Internal_FLASH_Read(1);
+	}
+
+	while (1)
+		;
 
 	return 0;
 }
 /*
-void RGB() {
+ void RGB() {
 
-}
-*/
+ }
+ */
