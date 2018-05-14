a214766be19d1efef4f8b2bf28d9c9bf0f4924a5
DamianWiatrzyk <D.Wiatrzyk@hotmail.com>
2018-05-14 21:26:23 +0200

    wstępna logika

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@
  * @brief   Default main function.
  ******************************************************************************
  */
-
+#include "RFID_lib.h"
 #include <tm_stm32f4_mfrc522.h>
 #include "stm32f4xx.h"
 #include "stm32f4_discovery.h"
@@ -30,17 +30,42 @@ int main(void) {
 	 }
 	 }
 	 */
+	TM_MFRC522_Init();
 
 	servoInit();
-	openServo();
+	closeServo();
 
 	ledInit();
 	setLED(0);
 
+	//0->open 1->close
+	int stateLock=1;
+
+
 	while (1) {
 		//servoSetDuty(40);
-	}
 
+		if(stateLock==1){
+			//tryb otwartego sejfu
+
+		struct CardID currentCard=readCard();
+
+		if(checkCard(currentCard)==0){
+			openServo();
+			stateLock=1;
+		}
+	}
+		else if(stateLock==0){
+			//tryb serwisowy
+
+			/*tutaj wklep sobie przerwania(StasiaXD), którymi bedziesz coœ tam serwisowa³ i jakiœ licznik, ¿e niby jak
+			od 5 sekund jest zakmniêtye to sobie odpalaszto na dole
+			if(przytrzymaniePrzyciskuCzyCoœCoTamOgarnie¯eZamknalesSejfXD==1){
+			closeServo();
+			stateLock=1;
+			}*/
+		}
+	}
 	return 0;
 }
 
